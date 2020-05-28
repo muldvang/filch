25caee10fc234d7d7ae43866606dba28bae02547
Torben Muldvang Andersen - QIAGEN <Torben.Andersen@qiagen.com>
2020-05-28 10:42:12 +0200

    Show song immediately when starting the program.

--- filch/modules/mpd_module.py
+++ filch/modules/mpd_module.py
@@ -2,12 +2,21 @@ import time
 import subprocess
 
 def run(callback):
+    text = current()
+    if text:
+        callback(text='🎜 ' + text)
+    else:
+        callback(text='')
     while True:
-        text = current()
+        text = current_wait()
         if text:
             callback(text='🎜 ' + text)
         else:
             callback(text='')
 
 def current():
+    return subprocess.run(["mpc", "current"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+
+def current_wait():
     return subprocess.run(["mpc", "current", "--wait"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+
