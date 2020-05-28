aa01a26bc2dfd26ad639fbd5f2ee3d9bcae63e6d
Torben Muldvang Andersen - QIAGEN <Torben.Andersen@qiagen.com>
2020-05-28 11:04:52 +0200

    Show only song when it is playing.

--- filch/modules/mpd_module.py
+++ filch/modules/mpd_module.py
@@ -2,21 +2,25 @@ import time
 import subprocess
 
 def run(callback):
-    text = current()
-    if text:
+    update(callback)
+    while True:
+        wait_for_change()
+        update(callback)
+
+
+def update(callback):
+    if is_playing():
+        text = current()
         callback(text='🎜 ' + text)
     else:
         callback(text='')
-    while True:
-        text = current_wait()
-        if text:
-            callback(text='🎜 ' + text)
-        else:
-            callback(text='')
+
+def is_playing():
+    return '[playing]' in subprocess.run(["mpc"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
 
 def current():
     return subprocess.run(["mpc", "current"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
 
-def current_wait():
-    return subprocess.run(["mpc", "current", "--wait"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
 
+def wait_for_change():
+    subprocess.run(["mpc", "idle"])
