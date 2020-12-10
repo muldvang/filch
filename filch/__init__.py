b49cc212562e15ef46b69d6445af9ccb1466bad3
Torben Muldvang Andersen - QIAGEN <Torben.Andersen@qiagen.com>
2020-12-10 12:45:36 +0100

    Remove MPD module because I am not using it anymore

--- filch/__init__.py
+++ filch/__init__.py
@@ -4,14 +4,14 @@ import time
 from threading import Thread
 from . import updater
 from .modules import audio_module, time_module, dropbox_module, wlan_module, internet_module,\
-     battery_module, load_module, mpd_module
+     battery_module, load_module
 
 def run():
     sys.stdout.write('{"version":1}\n')
     sys.stdout.write('[\n')
     sys.stdout.write('[]\n')
 
-    MODULES = [dropbox_module, mpd_module, audio_module, wlan_module, internet_module, battery_module, load_module, time_module]
+    MODULES = [dropbox_module, audio_module, wlan_module, internet_module, battery_module, load_module, time_module]
     MODULE_NAMES = {module:'Module of ' + str(module) for module in MODULES}
 
     UPDATER = updater.Updater(MODULE_NAMES.values())
