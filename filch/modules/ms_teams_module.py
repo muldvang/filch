74a5602be7b3741736b2934c99688ffddf78409b
Torben Muldvang Andersen - QIAGEN <Torben.Andersen@qiagen.com>
2023-11-02 08:26:02 +0100

    Correct Window title

--- filch/modules/ms_teams_module.py
+++ filch/modules/ms_teams_module.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 def run(callback):
-    command = '''bash -c "swaymsg --type get_tree | grep 'Microsoft Teams Classic' | grep -o '([0-9+])' | head -n 1 | grep -o '[0-9+]'"'''
+    command = '''bash -c "swaymsg --type get_tree | grep 'Microsoft Teams classic' | grep -o '([0-9+])' | head -n 1 | grep -o '[0-9+]'"'''
     while True:
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
         if result.stdout:
